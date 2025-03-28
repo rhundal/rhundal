f27d3a43e605f6605c097f856f79b27460ed5391
Rajdeep H <hrajdeep712@gmail.com>
2025-03-28 03:43:01 -0500

    revert and test

--- info.js
+++ info.js
@@ -1,10 +1,8 @@
-fetch("https://api.ip-api.com/json/?fields=status,country,city")
-  .then(response => response.json())
+fetch("https://ipinfo.io/json?token=your_actual_token_here") 
+  .then(response => response.json()) 
   .then(data => {
-      if (data.status !== "success") throw new Error("Failed to fetch location");
-
       let visitorInfo = `${new Date().toLocaleString()} - ${data.city}, ${data.country}`;
-      
+
       let visitors = JSON.parse(localStorage.getItem("visitors")) || [];
       visitors.push(visitorInfo);
       localStorage.setItem("visitors", JSON.stringify(visitors));
@@ -15,3 +13,9 @@ fetch("https://api.ip-api.com/json/?fields=status,country,city")
       console.error("Error fetching location:", error);
       document.getElementById("visitor-list").innerHTML = "<li>Error loading visitors</li>";
   });
+
+// Function to display visitors on the page
+function displayVisitors(visitors) {
+    let visitorList = document.getElementById("visitor-list");
+    visitorList.innerHTML = visitors.map(v => `<li>${v}</li>`).join("");
+}
